4ae9202b25890beb7584fca754f65e9336a28ef2
LuisCanales <luis.canalesv@mail.udp.cl>
2018-06-07 00:10:30 -0400

    Configurando las rutas y controladores restantes

---
+++ resources/views/contacto.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+
+@section('content')
+    <h1>Insertar contacto</h1>
+@endsection
---
+++ resources/views/incidentes.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+
+@section('content')
+    <h1>AQUI VAN LOS INCIDENTES</h1>
+@endsection
